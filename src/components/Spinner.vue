85820d8c2bbd6c4812d67ef833317d14c5926489
parsehex <1779937+parsehex@users.noreply.github.com>
2020-12-04 19:09:18 -0500

    add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,23 @@
+<template>
+	<div :class="['spinner', type ? 'text-' + type : '']" role="status">
+		<span class="sr-only">Loading...</span>
+	</div>
+</template>
+
+<script lang="ts">
+	import { computed, defineComponent } from 'vue';
+	import { BootstrapType } from './types';
+
+	export default defineComponent({
+		name: 'Spinner',
+		props: {
+			type: {
+				type: String as () => BootstrapType,
+				default: '',
+			},
+		},
+		setup(props) {
+			return {};
+		},
+	});
+</script>
