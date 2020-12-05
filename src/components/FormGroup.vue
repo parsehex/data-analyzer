85b754ca6220cb9e4f3e75f9b33dd8c02169b634
parsehex <1779937+parsehex@users.noreply.github.com>
2020-12-04 19:09:03 -0500

    add formgroup

---
+++ src/components/FormGroup.vue
@@ -0,0 +1,16 @@
+<template>
+	<div :class="'form-group'">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'FormGroup',
+		setup(props) {
+			return {};
+		},
+	});
+</script>
