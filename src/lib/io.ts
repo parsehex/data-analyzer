0072df51b80755c049613cd3d5214b6886211fd1
parsehex <1779937+parsehex@users.noreply.github.com>
2020-12-08 18:11:22 -0500

    add function to load a spreadsheet from arraybuffer

---
+++ src/lib/io.ts
@@ -0,0 +1,35 @@
+import * as xlsx from 'xlsx';
+
+interface Options<Col> {
+	content: ArrayBuffer;
+	sheetName: string;
+	header?: xlsx.Sheet2JSONOpts['header'];
+	sort?: keyof Col | ((a: Col, b: Col) => number);
+}
+
+export function loadSpreadsheetFile<ColumnType>({
+	content,
+	sheetName,
+	header,
+	sort,
+}: Options<ColumnType>): ColumnType[] {
+	// convert spreadsheet to json
+	// const content = await getFileContent(file.file_id);
+	const wb = xlsx.read(new Uint8Array(content), {
+		type: 'array',
+	});
+	const sheetIndex = wb.SheetNames.indexOf(sheetName);
+	const data: ColumnType[] = xlsx.utils.sheet_to_json(
+		wb.Sheets[wb.SheetNames[sheetIndex]],
+		{ header }
+	);
+
+	if (typeof sort === 'string') {
+		// @ts-ignore
+		data.sort((a, b) => a[sort] - b[sort]);
+	} else if (typeof sort === 'function') {
+		data.sort(sort);
+	}
+
+	return data;
+}
