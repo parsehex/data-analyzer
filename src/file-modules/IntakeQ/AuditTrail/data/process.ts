6ca36a07104f9aafafa42405ddbe0f8372cac740
parsehex <1779937+parsehex@users.noreply.github.com>
2021-02-02 16:16:25 -0500

    lint

--- src/file-modules/IntakeQ/AuditTrail/data/process.ts
+++ src/file-modules/IntakeQ/AuditTrail/data/process.ts
@@ -13,7 +13,6 @@ export default async function processData_IntakeQAuditTrail(
 		const newData = loadSpreadsheetFile<IntakeQRow_AuditTrail>({
 			buffer,
 			sheetName: 'export',
-			raw: true,
 		});
 		sheet.push(...newData);
 	}
